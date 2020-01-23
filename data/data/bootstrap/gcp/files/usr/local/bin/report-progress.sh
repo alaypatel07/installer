01b82d4515c612de4aedda94d3ffbebaef61f2d6
Alay Patel <alay1431@gmail.com>
2020-01-23 15:14:58 -0500

    bootkube.sh.template: stop gcp-routes.service after cvo-bootstrap

--- data/data/bootstrap/gcp/files/usr/local/bin/report-progress.sh
+++ data/data/bootstrap/gcp/files/usr/local/bin/report-progress.sh
@@ -9,11 +9,14 @@ wait_for_existance() {
 	done
 }
 
-echo "Waiting for bootstrap to complete..."
-wait_for_existance /opt/openshift/.bootkube.done
+echo "Waiting for cvo-bootstrap to complete..."
+wait_for_existance /opt/openshift/cvo-bootstrap.done
 
 ## remove the routes setup so that we can open up the blackhole
 systemctl stop gcp-routes.service
+
+echo "Waiting for bootstrap to complete..."
+wait_for_existance /opt/openshift/.bootstrap.done
 
 echo "Reporting install progress..."
 while ! oc --config="$KUBECONFIG" create -f - <<-EOF
